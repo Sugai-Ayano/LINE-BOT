662a0ea6a15d7ecee6b9ba22fab241f200a2fc31
Sugai-Ayano <b3w41067a@gmail.com>
2021-04-21 14:01:44 +0900

    [Add]line_bot_controller/署名の検証

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,14 +2,18 @@ class LineBotController < ApplicationController
   protect_from_forgery except:[:callback]
   def callback
     body = request.body.read
-    p body
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+     return head :bad_request
+    end
   end
 
   private
+ 
   def client
-    @client ||= LINE::Bot::Client.new{|config|
-      config.channel_secret=ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token=ENV["LINE_CHANNEL_TOKEN"]
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 end
